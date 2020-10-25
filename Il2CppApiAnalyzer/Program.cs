62443651dfe1dd447b5111ee112b4997545339c9
ghorsington <neigh@coder.horse>
2020-10-25 12:14:02 +0200

    Add initial API header analyzer project

---
+++ Il2CppApiAnalyzer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Il2CppApiAnalyzer
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
